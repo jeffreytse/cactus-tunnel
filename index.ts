ce4375eb23979f7cc4c2af0510be22c0d74edbeb
Jeffrey Tse <jeffreytse.mail@gmail.com>
2022-01-16 14:37:14 +0800

    feat: init commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import http from 'http';
+
+const requestListener: http.RequestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
